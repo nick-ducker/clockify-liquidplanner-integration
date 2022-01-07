416ce7b7dd1f17b04bb55d308b2537a96eb89635
Nick Ducker <nick@atomix.com.au>
2022-01-07 15:45:00 +1030

    add checkTimerEvent function

--- src/sundail/sundail.service.ts
+++ src/sundail/sundail.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class SundailService {}
+export class SundailService {
+
+  public checkTimerEvent(description: string): string {
+    const regexPattern = /(?<=\{)(.*?)(?=\})/
+    const match = description.match(regexPattern);
+    if (match) {
+      return match[0];
+    }
+    return ''
+  }
+}
