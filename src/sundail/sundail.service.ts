0c65936e54ba0e5269317a54826f4181c29d311d
Nick Ducker <nick@atomix.com.au>
2022-01-07 15:57:50 +1030

    add determineLpEntityId method

--- src/sundail/sundail.service.ts
+++ src/sundail/sundail.service.ts
@@ -11,4 +11,23 @@ export class SundailService {
     }
     return ''
   }
+
+  public async determineLpEntityId(lpFlag: string): Promise<string> {
+    const flagLength = lpFlag.split('').length
+    if(flagLength <= 5) {
+      console.log('Zoho ticket ID detected')
+      console.log('Searching LP using naming convention "ZOHO-${lpFlag}')
+
+      const { data } = await this.lp.getTasks({
+        filters: [{
+          filter: 'name',
+          operator: 'contains',
+          term: `ZOHO-${lpFlag}`
+        }]
+      })
+
+      return data.id
+
+    }
+  }
 }
